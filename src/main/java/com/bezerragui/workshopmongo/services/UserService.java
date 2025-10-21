5b678410632ac12383300a68f9c2878b698d6458
guilherme.bezerra <guilhermesbezerra1@gmail.com>
2025-10-20 22:48:21 -0300

    Obtendo um usuário por id

--- src/main/java/com/bezerragui/workshopmongo/services/UserService.java
+++ src/main/java/com/bezerragui/workshopmongo/services/UserService.java
@@ -1,22 +1,28 @@
 package com.bezerragui.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bezerragui.workshopmongo.domain.User;
 import com.bezerragui.workshopmongo.repository.UserRepository;
+import com.bezerragui.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findall() {
 		return repo.findAll();
-		
+
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
